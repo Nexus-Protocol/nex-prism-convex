44eb5d19fb07523e5e9db323c638c41b6e4424ec
Steven Li <steven4354@gmail.com>
2022-05-05 20:26:41 -0700

    unit tests

---
+++ contracts/nexus_prism_staking/tests/integration.rs
@@ -0,0 +1,66 @@
+use cosmwasm_std::Addr;
+use cosmwasm_std::testing::{mock_env, MockApi, MockQuerier, MockStorage};
+use terra_multi_test::{App, BankKeeper, ContractWrapper, Executor, TerraMockQuerier};
+use nexus_prism_staking::contract::{execute, instantiate, query};
+use nexus_prism_protocol::{
+    common::query_token_balance,
+    staking::{
+        AnyoneMsg, ExecuteMsg, GovernanceMsg, InstantiateMsg, MigrateMsg, QueryMsg,
+        RewardOperatorMsg, StakeOperatorMsg,
+    },
+};
+
+fn mock_app() -> App {
+    let api = MockApi::default();
+    let env = mock_env();
+    let bank = BankKeeper::new();
+    let storage = MockStorage::new();
+    let tmq = TerraMockQuerier::new(MockQuerier::new(&[]));
+
+    App::new(api, env.block, bank, storage, tmq)
+}
+
+fn init_contracts(app: &mut App) -> Addr {
+    let owner = Addr::unchecked("contract_owner");
+
+    // instantiate staking
+    let staking_contract = Box::new(ContractWrapper::new(
+        execute,
+        instantiate,
+        query,
+    ));
+
+    let staking_code_id = app.store_code(staking_contract);
+
+    let msg = InstantiateMsg{
+        governance: "governance_addr".to_string(),
+        staking_token: "governance_addr".to_string(),
+        stake_operator: None,
+        reward_token: "governance_addr".to_string(),
+        reward_operator: "governance_addr".to_string(),
+        xprism_token: None,
+        prism_governance: None,
+        nexprism_xprism_pair: None
+    };
+
+    let staking_instance = app
+        .instantiate_contract(
+            staking_code_id,
+            owner.clone(),
+            &msg,
+            &[],
+            String::from("STAKING"),
+            None,
+        )
+        .unwrap();
+
+    (
+        staking_instance
+    )
+}
+
+#[test]
+fn proper_initialization() {
+    let mut app = mock_app();
+    let (staking_instance) = init_contracts(&mut app);
+}
